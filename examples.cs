103642e1044b5c3bef04d525348fe59a16e0929c
moonstormm <mashasolo425@gmail.com>
2015-03-25 21:52:23 +0200

    IT3

---
+++ examples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class examples : Form
+    {
+        public examples()
+        {
+            InitializeComponent();
+        }
+
+        private void examples_Load(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+            try
+            {
+                webBrowser1.Navigate("www.google.com");             
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+    }
+}
